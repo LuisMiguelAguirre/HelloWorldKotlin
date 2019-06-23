44e7d0d46154e3117ee990036ecdaffeeca970d6
Luis Aguirre (Luis) <luis.aguirre@gm.com>
2019-06-22 23:11:35 -0400

    All versions

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -1,4 +1,37 @@
 fun main(args: Array<String>) {
-    val name: String = if (args.isNotEmpty()) args[0] else "Kotlin"
+    firstVersion()
+    secondVersion(args)
+    thirdVersion(args)
+    fourthVersion(args)
+    println("First ${foo()} Second ${foo()}")
+}
+
+fun firstVersion() {
+    val name = "Luis"
+    print("First version: ")
     println("Hello $name")
+}
+
+fun secondVersion(args: Array<String>) {
+    //TODO refactor name in third version
+    val name = if (args.size > 0) args[0] else "Kotlin"
+    print("Second version: ")
+    println("Hello $name")
+}
+
+fun thirdVersion(args: Array<String>) {
+    val name = if (args.isNotEmpty()) args[0] else "Kotlin"
+    print("Third version: ")
+    println("Hello world $name")
+}
+
+fun fourthVersion(args: Array<String>) {
+    print("Fourth version: ")
+    //println("Hello $args.getOrNull(0)") wrong, functions must be in curly braces
+    println("Hello ${args.getOrNull(0)}")
+}
+
+fun foo(): String {
+    println("Returning calculating foo...")
+    return "foo"
 }
