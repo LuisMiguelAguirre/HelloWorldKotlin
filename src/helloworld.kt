c582d06ad721c8eb545af0d0341dd86fc5cb57d3
Luis Aguirre (Luis) <luis.aguirre@gm.com>
2019-06-22 22:55:33 -0400

    Change conditional with .isNotEmpty method

--- src/helloworld.kt
+++ src/helloworld.kt
@@ -1,4 +1,4 @@
 fun main(args: Array<String>) {
-    val name: String = if (args.size > 0) args[0] else "Kotlin"
+    val name: String = if (args.isNotEmpty()) args[0] else "Kotlin"
     println("Hello $name")
 }
